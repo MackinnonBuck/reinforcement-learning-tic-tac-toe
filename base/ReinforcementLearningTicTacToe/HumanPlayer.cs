4ec7ed8c03d7f95e9cb752ae73a313ec3740f49a
Camden Stocker <cstocker236@gmail.com>
2020-11-22 14:21:27 -0800

    Strip down HumanPlayer.cs for baseCode

--- base/ReinforcementLearningTicTacToe/HumanPlayer.cs
+++ base/ReinforcementLearningTicTacToe/HumanPlayer.cs
@@ -20,21 +20,14 @@ namespace ReinforcementLearningTicTacToe
                 input = Console.ReadLine();
 
                 if (int.TryParse(input, out move)) {
-                    // TODO: Check if number is between 1 and 9, otherwise, ask again
-
-                    //move = (int) input;
-
                     if (move >= 1 && move <= 9 && state.Contains(input)) {
                         break;
                     }
-
                 }
                 Console.WriteLine("Invalid Move.");
                 Console.WriteLine("Choose move 1-9: ");       
-                
             }
 
-            //return state.Replace(Convert.ToChar(move), Tag);
             return state.Replace(input[0], Tag);
         }
     }
