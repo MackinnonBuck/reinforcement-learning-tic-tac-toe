7947d0c7bef82c5c2a0b286b363e3d38f29af36b
Camden Stocker <cstocker236@gmail.com>
2020-11-22 14:39:36 -0800

    Strip down Program.cs

--- base/ReinforcementLearningTicTacToe/Program.cs
+++ base/ReinforcementLearningTicTacToe/Program.cs
@@ -4,23 +4,6 @@
     {
         private static void Main()
         {
-            // Training
-            DeepAgent player1 = new DeepAgent('X', 1.0f);
-            DeepAgent player2 = new DeepAgent('O', 0.0f);
-
-            var game = new Game(player1, player2);
-            game.PlayToLearn(50);
-            player1.SaveModel();
-            player2.SaveModel();
-
-            // Playing
-            //DeepAgent player1 = new DeepAgent('X', 0.8f);
-            //HumanPlayer player2 = new HumanPlayer('O');
-
-            //var game = new Game(player1, player2);
-            //game.PlayGame();
-
-            //player1.SaveModel();
         }
     }
 }
